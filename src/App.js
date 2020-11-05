60e9b8ff6454af44d88e69626baf5c4dc199284f
syedKhutub <syedkhutubsks@gmail.com>
2020-11-05 17:44:36 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import AppBar from './components/HeaderUI'
+// import Body from './components/BodyUI'
+import SearchField from './containers/SearchField'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchField />
+      {/* <Body/> */}
     </div>
   );
 }
